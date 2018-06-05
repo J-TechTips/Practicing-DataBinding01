d676160e75bdb57031a3b7486829b548ba2c82f3
Jonathan <jtecktips@gmail.com>
2018-06-05 15:13:54 -0700

    Creating Person Details and having them display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Person } from './classes/person';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+ title = 'app';
+ heading = 'Person Details';
+ person: any;
+ public myPeeps: Person = new Person();
+ result: any[];
+
+ constructor() {
+   console.log(this.myPeeps.peoples);
+   this.result = this.myPeeps.peoples;
+ }
 }
---
+++ src/app/classes/person.ts
@@ -0,0 +1,16 @@
+export class Person {
+  public peoples: any[] = [{
+      gender: 'Mr.',
+      firstName : 'John',
+      lastName : 'Doe',
+      age : 25,
+      city: 'Boston'
+  }, {
+      gender: 'Ms.',
+      firstName : 'Emily',
+      lastName : 'Smith',
+      age : 'Deceased',
+      city: 'NYC'
+  } ];
+  public doThings() {}
+}
